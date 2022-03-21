0a2b912cad5f955df847ce3ee78525a2ade40f23
Andy Lammerts <andylammerts@outlook.com>
2022-03-21 11:04:04 +0100

    Inleveropdracht B af

---
+++ Inleveropdracht B/js/script.js
@@ -0,0 +1,9 @@
+var app = angular.module('autoApp', []);
+app.controller('autoCtrl', function($scope) {
+  $scope.autos = [
+    {kenteken:'34-E5-AS',kleur:'rood',merk:'audi',deuren:'4',prijs:'50000'},
+    {kenteken:'18-ZM-VV',kleur:'groen',merk:'volkswagen',deuren:'2',prijs:'11'},
+    {kenteken:'22-AA-44',kleur:'zwart',merk:'bmw',deuren:'1',prijs:'44444'}
+    ];
+
+});
