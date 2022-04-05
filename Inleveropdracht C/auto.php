3aee11a1a503aa2bce57afe4ba3a77dc633d0cf6
AndyLammerts <124131@student.drenthecollege.nl>
2022-04-05 13:20:04 +0200

    Inleveropdracht C af

--- Inleveropdracht C/auto.php
+++ Inleveropdracht C/auto.php
@@ -1,15 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-$conn = new mysqli("localhost", "C4", "C4", "bedrijf");
+$conn = new mysqli("localhost", "root", "", "bedrijf");
 
 $result = $conn->query("SELECT Bedrijfsnaam, Adres, Woonplaats, Telefoon FROM bedrijf");
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
- if ($outp != "") {$outp .= ",";}
- $outp .= '{"Bedrijfsnaam":"' . $rs["Bedrijfsnaam"] . '",';
- $outp .= '"Adres":"' . $rs["Adres"] . '",';
- $outp .= '"Telefoon":"'. $rs["Telefoon"] . '"}';
+ if ($outp != "") {$outp .= ",".PHP_EOL;}
+ $outp .= '{"Bedrijfsnaam":"' . $rs["Bedrijfsnaam"] . '",'.PHP_EOL;
+ $outp .= '"Adres":"' . $rs["Adres"] . '",'.PHP_EOL;
+ $outp .= '"Telefoon":"'. $rs["Telefoon"] . '"}'.PHP_EOL;
 }
 $outp ='{"records":['.$outp.']}';
 $conn->close();
