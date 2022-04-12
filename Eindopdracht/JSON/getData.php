6367110415818969786eecd00c42dc3feb2930fd
AndyLammerts <124131@student.drenthecollege.nl>
2022-04-12 13:10:25 +0200

    Back-up eindopdracht

--- Eindopdracht/JSON/getData.php
+++ Eindopdracht/JSON/getData.php
@@ -3,11 +3,12 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 $conn = new mysqli("localhost", "root", "", "C4");
 
-$result = $conn->query("SELECT Voornaam, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Telefoonnummer FROM personen");
+$result = $conn->query("SELECT ID, Voornaam, Achternaam, Straat, Huisnummer, Postcode, Woonplaats, Telefoonnummer FROM personen");
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
  if ($outp != "") {$outp .= ",".PHP_EOL;}
-   $outp .= '{"Voornaam":"' . $rs["Voornaam"] . '",'.PHP_EOL;
+    $outp .= '{"ID":"' . $rs["ID"] . '",'.PHP_EOL;
+   $outp .= '"Voornaam":"' . $rs["Voornaam"] . '",'.PHP_EOL;
    $outp .= '"Achternaam":"' . $rs["Achternaam"] . '",'.PHP_EOL;
    $outp .= '"Straat":"' . $rs["Straat"] . '",'.PHP_EOL;
    $outp .= '"Huisnummer":"' . $rs["Huisnummer"] . '",'.PHP_EOL;
--- Eindopdracht/script.js
+++ Eindopdracht/script.js
@@ -1,6 +1,24 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ["ngRoute"]);
 app.controller('customersCtrl', function($scope, $http) {
-  $http.get("getData.php").then(function(response) {
+  $http.get("JSON/getData.php").then(function(response) {
     $scope.bedrijf = response.data.records;
   });
+
+  app.config(function($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl : "routes/main.html"
+    })
+    .when("/add", {
+      templateUrl : "routes/add.html"
+    })
+    .when("/edit", {
+      templateUrl : "edit.html",
+    });
+  });
+
+  $scope.edit = function(index) {
+    console.log("edit="+index);
+
+  }
 });
